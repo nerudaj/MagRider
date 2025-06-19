c493e751b1c3d85447953b8483be8fe8d4aa010f
Jakub Neruda <nerudaj@email.cz>
2025-06-19 21:17:41 +0200

    Cleanup dummy impl

--- lib/src/game/engine/RenderingEngine.cpp
+++ lib/src/game/engine/RenderingEngine.cpp
@@ -7,13 +7,6 @@ void RenderingEngine::update(const dgm::Time& time)
 
 void RenderingEngine::draw(dgm::Window& window)
 {
-    sprite.setTextureRect(scene.dummy.animation.getCurrentFrame());
-    sprite.setPosition(scene.dummy.body.getCenter());
-    sprite.setScale({ scene.dummy.facingLeft ? -1.f : 1.f, 1.f });
-
-    scene.dummy.body.debugRender(window); // rendering hitbox
-    window.draw(sprite);
-
     text.setString(fpsCounter.getText());
     window.draw(text);
 }

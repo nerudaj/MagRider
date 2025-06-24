f1def46d52e85fa366178a4df7714487b9dd325a
Jakub Neruda <nerudaj@email.cz>
2025-06-24 21:28:47 +0200

    Add Tiled project

---
+++ assets-private/tiled/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="32" tileheight="32" spacing="4" tilecount="36" columns="6">
+ <image source="../../assets/graphics/set.png" width="212" height="212"/>
+</tileset>

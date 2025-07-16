4d405990958a9dcf548da4d037c56f5ef504db31
Jakub Neruda <nerudaj@email.cz>
2025-07-16 22:06:07 +0200

    Add level 19

---
+++ assets-private/tiled/Metal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Metal" tilewidth="32" tileheight="32" spacing="4" tilecount="42" columns="6">
+ <image source="../../assets/graphics/set2.png" width="212" height="248"/>
+</tileset>
